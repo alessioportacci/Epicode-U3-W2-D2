3e480f3f085c1badce78cd600eaa53cbe26a4877
alessioportacci <alessio.portacci.s@gmail.com>
2023-07-26 21:36:21 +0200

    Aggiunte le routes e le pagine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ActivePostComponent } from './components/pages/active-post/active-post.component';
+import { InactivePostComponent } from './components/pages/inactive-post/inactive-post.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {path: '', component: HomeComponent},
+  {path: 'active-post', component: ActivePostComponent},
+  {path: 'inactive-post', component: InactivePostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ActivePostComponent } from './components/pages/active-post/active-post.component';
+import { InactivePostComponent } from './components/pages/inactive-post/inactive-post.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ActivePostComponent,
+    InactivePostComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
